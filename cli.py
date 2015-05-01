3f0af6a2090bb772cb1231905f36019fa2ac7b94
Ranmocy Sheng <ranmocy@gmail.com>
2015-05-01 13:11:37 -0700

    add readline history and pre-import rdd classes

--- cli.py
+++ cli.py
@@ -6,6 +6,27 @@ import rlcompleter
 from gevent import fileobject
 
 
+# Save readline history
+# https://docs.python.org/2/tutorial/interactive.html
+import atexit
+import os
+import readline
+import rlcompleter
+
+historyPath = os.path.expanduser("~/.pyhistory")
+
+def save_history(historyPath=historyPath):
+    import readline
+    readline.write_history_file(historyPath)
+
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
+atexit.register(save_history)
+del os, atexit, readline, rlcompleter, save_history, historyPath
+
+
+# Interactive CLI
 class CLI(code.InteractiveConsole):
     def __init__(self, locals=None, filename="<console>", histfile=None):
         self._green_stdin = fileobject.FileObject(sys.stdin)
@@ -23,6 +44,10 @@ class CLI(code.InteractiveConsole):
             except ImportError:
                 pass
             readline.parse_and_bind("tab: complete")
+
+        # Preload rdds
+        self.runcode(code.compile_command("from rdd import *"))
+
         self.interact("Welcome to SparkP2P!")
 
     # def raw_input(self, prompt):
