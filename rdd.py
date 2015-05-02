399459da2e7896d6fa17854167c5788fb5c477f3
Ranmocy Sheng <ranmocy@gmail.com>
2015-05-01 22:10:55 -0700

    add partitions support

--- rdd.py
+++ rdd.py
@@ -1,21 +1,43 @@
+import os
 import StringIO
 import cloudpickle
+
+
+PARTITION_NUM = 2
 
 
 class Context(object):
     def __init__(self, worker=None, driver=None):
         self.worker = worker
         self.driver = driver
-        if not self.driver:
+        if self.driver:
             self.workers = driver.workers
+            PARTITION_NUM = len(self.workers)
 
     def textFile(self, filename):
         return TextFile(self, filename)
 
 
+class Partition(object):
+    """`part_id` starts from 0"""
+    def __init__(self, rdd, part_id):
+        super(Partition, self).__init__()
+        self.rdd = rdd
+        self.part_id = part_id
+        self.data = None
+
+    def get(self):
+        if self.data is None:
+            self.data = []
+            for element in self.rdd.get(self.part_id):
+                self.data.append(element)
+        return self.data
+
+
 class RDD(object):
     def __init__(self, context):
         self.context = context
+        self.partitions = [Partition(self, i) for i in range(PARTITION_NUM)]
 
     def dump(self):
         output = StringIO.StringIO()
@@ -31,17 +53,15 @@ class RDD(object):
 
     def collect(self):
         elements = []
-        while True:
-            element = self.get()
-            if element is None:
-                break
-            elements.append(element)
+        for partition in self.partitions:
+            for element in partition.get():
+                elements.append(element)
         return elements
 
     def count(self):
         return len(self.collect())
 
-    def get(self):
+    def get(self, part_id):
         raise Exception("Need to be implemented in subclass.")
 
 
@@ -49,20 +69,24 @@ class TextFile(RDD):
     def __init__(self, context, filename):
         super(TextFile, self).__init__(context)
         self.filename = filename
-        self.lines = None
-        self.index = 0
 
-    def get(self):
-        if not self.lines:
-            with open(self.filename) as handler:
-                self.lines = handler.readlines()
+    def get(self, part_id):
+        size = os.path.getsize(self.filename)
+        length = size / PARTITION_NUM
+        offset = length * part_id
+        if part_id is PARTITION_NUM - 1:  # last one
+            length = size - offset
 
-        if self.index == len(self.lines):
-            return None
-        else:
-            line = self.lines[self.index]
-            self.index += 1
-            return line
+        with open(self.filename) as handler:
+            handler.seek(offset)
+            if part_id is not 0:  # unless it's first one, ignore the first line
+                handler.readline()
+            while length >= 0:  # read lines until to the end
+                line = handler.readline()
+                if len(line) is 0:  # reach the end of file
+                    break
+                length -= len(line)
+                yield line
 
 
 class Map(RDD):
@@ -71,13 +95,9 @@ class Map(RDD):
         self.parent = parent
         self.func = func
 
-    def get(self):
-        element = self.parent.get()
-        if element is None:
-            return None
-        else:
-            element_new = self.func(element)
-            return element_new
+    def get(self, part_id):
+        for element in self.parent.get(part_id):
+            yield self.func(element)
 
 
 class Filter(RDD):
@@ -86,14 +106,10 @@ class Filter(RDD):
         self.parent = parent
         self.func = func
 
-    def get(self):
-        while True:
-            element = self.parent.get()
-            if element is None:
-                return None
-            else:
-                if self.func(element):
-                    return element
+    def get(self, part_id):
+        for element in self.parent.get(part_id):
+            if self.func(element):
+                yield element
 
 
 if __name__ == '__main__':
