e6060c47c567f99a95d2e5e3dc89c10b38ac778c
Ranmocy Sheng <ranmocy@gmail.com>
2015-05-11 09:14:51 -0700

    update rdd demo code

--- myscript.py
+++ myscript.py
@@ -1,2 +1,2 @@
-f = context.text_file('myfile').map(lambda s: s.split()).filter(lambda a: int(a[1]) > 2)
+f = context.text_file('myfile').flat_map(lambda line: line.split()).filter(lambda item: item.isalpha()).map(lambda item: (item, 1)).reduce_by_key(lambda result, item: result + item)
 print f.collect()
--- rdd.py
+++ rdd.py
@@ -303,4 +303,7 @@ if __name__ == '__main__':
         .filter(lambda item: item.isalpha())\
         .map(lambda item: (item, 1))\
         .reduce_by_key(lambda result, item: result + item)
+
+    f = context.text_file('myfile').flat_map(lambda line: line.split()).filter(lambda item: item.isalpha()).map(lambda item: (item, 1)).reduce_by_key(lambda result, item: result + item)
+
     print f.collect()
